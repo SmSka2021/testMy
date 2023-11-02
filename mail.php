1a8a3e1fe54fa40fe3d2f57b8cdfc31d55a81154
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2023-11-02 15:58:45 +0300

    first commit

--- mail.php
+++ mail.php
@@ -1,14 +1,3 @@
-<!-- <?php
-$to = "Sve-Mac@yandex.ru"; // емайл получателя данных из формы // info@domain
-$them = "You have received a letter from a site user"; // тема полученного емайла
-$message = "Name User: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message .= "Phone: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-//$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
-$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $them, $message, $headers); //отправляет получателю на емайл значения переменных
-?> -->
 <?php
 $fio = $_POST['name'];
 $email = $_POST['email'];
